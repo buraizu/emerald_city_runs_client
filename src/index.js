729705c907db0576c0666bc83f33c1c02eec8438
Bryce Eadie <bryce.eadie@gmail.com>
2019-05-07 13:59:05 -0700

    Remove compose from index.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index.js';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk)
-  )
-)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
